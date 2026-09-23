ddcb845b498d56e154cacba29c5138a8d4e30629
markspl <14055798+markspl@users.noreply.github.com>
2026-09-24 01:11:34 +0300

    v0.0.1

---
+++ CHANGELOG.md
@@ -0,0 +1,4 @@
+## v0.0.1 (2026-09-24)
+
+### What's Changed
+* Initial release of `timelog-cli`
--- timelog/__init__.py
+++ timelog/__init__.py
@@ -1,3 +1,3 @@
 """timelog-cli: simple time/task logging on the command line."""
 
-__version__ = "0.0.0"
+__version__ = "0.0.1"
